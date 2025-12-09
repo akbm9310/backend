474217277ab23960442f179acc662186f9feec9e
Akash <akbm95326@gmail.com>
2025-12-09 09:19:00 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-//require('dotenv').config({path: './env'});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./.env",
 });
@@ -12,26 +12,5 @@ connectDB()
     });
   })
   .catch((error) => {
-    console.log("mongo db connection failed", err);
+    console.log("mongo db connection failed", error);
   });
-
-/*
-import express from "express";
-const app=express();
-
-(async()=>{
-    try{
-      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-      app.on("error",(error)=>{
-        console.error("Failed to connect to the database");
-        throw error;
-      })
-      app.listen(process.env.PORT,()=>{
-        console.log(`APP is listening on port ${process.env.PORT} `);
-        
-      })
-    }
-    catch(error){
-        console.error("Error connecting to the database", error);
-    }
-})()*/
